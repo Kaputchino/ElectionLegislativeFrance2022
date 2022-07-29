319b38601b12d89b8f7abe114927e6e412bdb006
Kaputchino <61330453+Kaputchino@users.noreply.github.com>
2022-07-29 23:40:18 +0200

    Update commande.sql

--- commande/commande.sql
+++ commande/commande.sql
@@ -18,3 +18,14 @@ inner join gouv2 on monde.Nom=gouv2.nom and monde.Prénom=gouv2.prenom and monde
 left join circonscription on gouv2.nom=circonscription.nom and gouv2.prenom=circonscription.prenom and gouv2.nuance=circonscription.nuance
 GROUP by nuancefilles.nomMere
 order by sum(gouv2.voix)
+
+
+/*renvoie la liste des partis d'un groupe et leur stat*/
+
+SELECT unitedpartiestable.nameParty  as "partie", sum(gouv2.voix) as "voix", count(circonscription.nuance) as "siege" FROM `monde`
+inner join unitedpartiestable on monde.id=unitedpartiestable.monde_id
+inner join gouv2 on monde.Nom=gouv2.nom and monde.Prénom=gouv2.prenom and monde.`Nuance Ministère`=gouv2.nuance
+left join circonscription on gouv2.nom=circonscription.nom and gouv2.prenom=circonscription.prenom and gouv2.nuance=circonscription.nuance
+where unitedpartiestable.nameParty like "%RPS"
+group by unitedpartiestable.nameParty  
+ORDER BY `voix` ASC
